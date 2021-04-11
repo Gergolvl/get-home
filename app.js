d28b968eb50ea1892ce2d2c805b32b5fb8971f33
Gergolvl <70323912+Gergolvl@users.noreply.github.com>
2021-04-11 12:19:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ function calculateFuelToOffload(totalDistanceToTravel, averageLitersOverHundred)
     // 1. lépés: Számolja ki a szükséges üzemanyag-mennyiséget.
     var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
     console.log('Szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L');
-
+    
+    var fuelToOffload = tankCapacity - requiredFuel;
+    console.log('Leeresztendő üzemanyag: ' + fuelToOffload + ' L');
     // 2. lépés: Számolja ki a lecsapolandó üzemanyagot.
 }
